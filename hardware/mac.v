53cb4257f981e1b7e9b25d5882164dc6b29aa4bd
Cj Alonso <135866734+sahota1@users.noreply.github.com>
2023-11-02 15:50:48 -0700

    Update mac.v

--- hardware/mac.v
+++ hardware/mac.v
@@ -5,8 +5,13 @@ module mac (out, a, b, c);
 parameter bw = 4;
 parameter psum_bw = 16;
 
+  input [bw-1] a;
+  input [bw-1] b;
+  input [psum_bw-1] c;
+  output signed [psum_bw-1] c;
+  assign out = c
 ...
-  out = a*b + c
+  out = a*b
 
   out
 
